348bc9fbb928b4b011551b772d079a2161f9a452
mohits0108 <mohits0108@gmail.com>
2024-10-16 10:02:06 +0530

    added reload function

--- src/contanier/Reports.jsx
+++ src/contanier/Reports.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 const Reports = () => {
+  useEffect(() => {
+    window.location.reload();
+  }, []); // The empty dependency array ensures it runs only once when the component mounts
+
   return (
     <div>
-      
+      {/* Your content goes here */}
     </div>
-  )
-}
+  );
+};
 
-export default Reports
+export default Reports;
+
